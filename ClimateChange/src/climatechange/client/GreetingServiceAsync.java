2ccf2f128cef69b6f5819a4b1314bf55a02016f2
Phhofm <philip.hofmann@uzh.ch>
2016-10-26 14:27:14 +0200

    initial setup with sample code
    This is simply the Ecplise workspace, with a nwely created GWT Web App
    with sample code included

---
+++ ClimateChange/src/climatechange/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package climatechange.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}
